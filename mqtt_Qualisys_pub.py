d1aa90d23151e8d515dfa2a5a1fe18fa594782a3
devriesusna <35604535+devriesusna@users.noreply.github.com>
2020-11-17 21:22:51 -0500

    Initial commit

---
+++ mqtt_Qualisys_pub.py
@@ -0,0 +1,65 @@
+import json
+from time import sleep
+from datetime import datetime
+import paho.mqtt.client as MQTT
+from gc import collect as trash
+import asyncio
+import qtm
+
+client = MQTT.Client("qtm_pub")
+
+pub_topics = {
+	'timestamp',
+	'bodyA',
+        'bodyB',
+        'bodyC',
+        'bodyD'
+}
+
+example_dict = {
+	'x':'0',
+	'y':'1',
+	'z':'2'
+}
+
+def on_packet(packet):
+    """ Callback function that is called everytime a data packet arrives from QTM """
+    global pub_topics
+    print("Framenumber: {}".format(packet.framenumber))
+    [header, bodies] = packet.get_6d()
+    print("Component info: {}".format(header))
+    print(type(bodies))
+    print("break","\t")
+    for body in bodies:
+        tmp = {"x":str(body[0][0]),"y":str(body[0][1]),"z":str(body[0][2])}
+        print("\t\n",body[0][0],"\t\n")
+        print(body[1])
+        print("\t\n","break")
+        client.publish('bodyA',json.dumps(tmp))
+#        client.publish('x_position',str(body[0][0]))
+ #       client.publish('y_position',str(body[0][1]))
+  #      client.publish('z_position',str(body[0][2]))
+
+
+
+async def setup():
+    """ Main function """
+    try:
+            client.connect('127.0.0.1')
+            print("Connected to MQTT broker")
+    except:
+            print("didn't connect")
+    connection = await qtm.connect("10.0.0.118")
+    if connection is None:
+        return
+
+    await connection.get_parameters(parameters=["6d"])
+    
+    await connection.stream_frames(components=["6d"], on_packet=on_packet)
+
+
+if __name__ == "__main__":
+    asyncio.ensure_future(setup())
+    asyncio.get_event_loop().run_forever()
+
+
---
+++ mqtt_Qualisys_sub.py
@@ -0,0 +1,103 @@
+from time import sleep
+import paho.mqtt.client as MQTT
+import logging
+import logging.handlers
+from datetime import datetime
+from gc import collect as trash
+import json
+import os
+from server import *
+
+DEBUGGING=1
+
+#                       #
+#-----Logging Setup-----#
+#                       #
+filename='/Users/Levi DeVries/Downloads/mqtt_1.log'
+log = logging.getLogger()
+log.setLevel(logging.INFO)
+format = logging.Formatter('%(asctime)s : %(message)s')
+file_handler = logging.FileHandler(filename)
+file_handler.setLevel(logging.INFO)
+file_handler.setFormatter(format)
+log.addHandler(file_handler)
+
+#                       #
+#-------MQTT Setup------#
+#                       #
+client=MQTT.Client(clientname)
+topiclist=[
+	'timestamp',
+        'bodyA',
+        'bodyB',
+        'bodyC',
+        'bodyD'
+]
+
+#a basic callback for MQTT that prints message data directly.
+def print_message(client,userdata,message):
+	print()
+	print("mqtt rx:")
+	print(message.topic)
+	print(message.qos)
+	print(message.payload)
+	print(message.payload.decode())
+	print(message.retain)
+	print(client)
+
+# A basic callback for MQTT that stores message data to a log file.
+def log_message(client,userdata,message):
+	log.info("message rx")
+	log.info(str(message.topic)+", "+str(message.payload))
+
+# The callback that our program will use to control device.
+def process(client,userdata,message):
+	#
+	# Code to use the mqtt data.
+	#
+	a = 1
+
+def check_quit():
+	return 1
+
+def setup_subscription():
+	check=0
+	try:
+		# Connect to the server (defined by server.py)
+		client.connect(server)
+
+		# Assigns the callback function when a mqtt message is received.
+		if (DEBUGGING):
+			client.on_message=print_message
+		else:
+			client.on_message=process
+
+		# Subscribes to all the topics defined at top.
+		for i in topiclist:
+			client.subscribe(i)
+
+		# Start the mqtt subscription.
+		client.loop_start()
+		log.info("mqtt subscription script started")
+		check=1
+	except:
+		print("didn't connect")
+		log.info("mqtt subscription failed")
+	return check
+
+def main():
+	q=0
+	# Run the MQTT setup once.
+	q=setup_subscription()
+
+	# Because the subscription works on interrupt callbacks, nothing happens in main.
+	while(q):
+		trash()
+		sleep(1)
+		q=check_quit()
+
+main()
+
+
+
+
